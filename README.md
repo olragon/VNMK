3f7ce89be1ff2ff020ca86c37890bc0f163a0bbc
olragon <olragon@gmail.com>
2011-05-04 18:06:38 +0700

    Use Riak to store data. Update document.

--- README.md
+++ README.md
@@ -1,14 +1,24 @@
-# Requires
-1. NPM: Node package management
-   Homepage: http://npmjs.org/
-   Document: https://github.com/isaacs/npm#readme
-   Install: curl http://npmjs.org/install.sh | sh
-2. Express: High performance, high class web development for Node.js
-   Homepage: http://expressjs.com
-   Document: http://expressjs.com/guide.html
-   Install: npm install express
-3. Nodemon: Monitor for any changes in your node.js application and automatically restart the server - perfect for development
-   Homepage: https://github.com/remy/nodemon
-   Document: https://github.com/remy/nodemon#readme
-   Install: sudo npm install -g nodemon
-
+# I. Requires
+## 1. Node.js
+### Node
+      1. 
+      2. 
+### Node's library
+      1. NPM: Node package management
+         Homepage: http://npmjs.org/
+         Document: https://github.com/isaacs/npm#readme
+         Install: curl http://npmjs.org/install.sh | sh
+      2. Express: High performance, high class web development for Node.js
+         Homepage: http://expressjs.com
+         Document: http://expressjs.com/guide.html
+         Install: npm install express
+      3. Nodemon: Monitor for any changes in your node.js application and automatically restart the server - perfect for development
+         Homepage: https://github.com/remy/nodemon
+         Document: https://github.com/remy/nodemon#readme
+         Install: sudo npm install -g nodemon
+## 2. Riak
+         Homepage: http://www.basho.com/
+         Document: http://wiki.basho.com/
+         Install:
+          `wget http://downloads.basho.com/riak/riak-0.14/riak_0.14.0-1_amd64.deb`
+          `sudo dpkg -i riak*.deb`
--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,51 @@ var app = express.createServer(
   express.bodyParser()
 );
 
+/**
+ * Định nghĩa các thuộc tính để tìm kiếm bài hát.
+ *   - Chú trọng vào các thuộc tính của <b>file nhạc</b>,
+ *   các thông tin như lời bài hát,
+ *   thông tin ca sĩ, năm phát hành albums, 
+ *   ngôn ngữ ... có thể bổ sung sau.
+ */
+var searchKeywords = new Array(
+  'singers', // Tên ca sĩ. Ví dụ: Quang Dũng, Bằng Kiều
+  'albums', // Tên album. Ví dụ: The Best Of Quang Dung 1, The Best Of Quang Dung 2
+  'songs', // Tên bài hát. Ví dụ: Bâng Khuâng Chiều Nội Trú, Anh Xin Làm
+  'genes', // Thể loại nhạc. Ví dụ: Pop, Rock, ...
+  'sources', // Nguồn của file nhạc,thường là tên domain. Ví dụ: mp3.zing.vn
+  'quanlities', // Chất lượng file nhạc. Ví dụ: 32, 64, >=128 
+  'formats', // Định dạng của file nhạc. Ví dụ: mp3, wav, ogg
+  'sorts', // Tiêu chí sắp xếp. Ví dụ: 
+  'limits' // Giới hạn số lượng kết quả. Ví dụ: 1..15
+);
+
+/**
+ * Build search query base on searchKeywords array.
+ * 
+ * @param searchKeywords
+ *   - Array contain search supported keywords
+ * @return query
+ *   - Query url
+ */
+function buildSearchQuery(searchKeywords){
+  query = new String;
+  
+  for (var i=0; i < searchKeywords.length; i++) {
+    // Example: keywords=:keywords?
+    query += searchKeywords[i] + '=:' + searchKeywords[i] + '?';
+    
+    if(i != searchKeywords.length - 1) {
+      query += '&';
+    }
+  };
+
+  return query;
+}
+
+/**
+ * Define menu route
+ */
 app.get('/', function(req, res){
   res.send('hello world');
 });
@@ -40,47 +85,6 @@ app.get('/vnmk/api/album/:albums', function(req, res){
 // app.get('/vnmk/api/search/:keywords', function(req, res){
   // res.send('keywords: ' + req.params.keywords);
 // });
-
-/**
- * Định nghĩa các thuộc tính để tìm kiếm bài hát.
- *   - Chú trọng vào các thuộc tính của <b>file nhạc</b>,
- *   các thông tin như lời bài hát,
- *   thông tin ca sĩ, năm phát hành albums, 
- *   ngôn ngữ ... có thể bổ sung sau.
- */
-var searchKeywords = new Array(
-  'singers', // Tên ca sĩ. Ví dụ: Quang Dũng, Bằng Kiều
-  'albums', // Tên album. Ví dụ: The Best Of Quang Dung 1, The Best Of Quang Dung 2
-  'songs', // Tên bài hát. Ví dụ: Bâng Khuâng Chiều Nội Trú, Anh Xin Làm
-  'genes', // Thể loại nhạc. Ví dụ: Pop, Rock, ...
-  'sources', // Nguồn của file nhạc,thường là tên domain. Ví dụ: mp3.zing.vn
-  'quanlities', // Chất lượng file nhạc. Ví dụ: 32, 64, >=128 
-  'formats', // Định dạng của file nhạc. Ví dụ: mp3, wav, ogg
-  'sorts', // Tiêu chí sắp xếp. Ví dụ: 
-  'limits' // Giới hạn số lượng kết quả. Ví dụ: 1..15
-);
-
-/**
- * Build search query base on searchKeywords array.
- * 
- * @param searchKeywords
- *   - Array contain search supported keywords
- * @return query
- *   - Query url
- */
-function buildSearchQuery(searchKeywords){
-  query = new String;
-  
-  for (var i=0; i < searchKeywords.length; i++) {
-    query += searchKeywords[i] + '=:' + searchKeywords[i] + '?';
-    
-    if(i != searchKeywords.length - 1) {
-      query += '&';
-    }
-  };
-
-  return query;
-}
 
 /**
  * Menu router for full search operation
