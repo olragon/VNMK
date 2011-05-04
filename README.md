0e2e6d57cbe06352ccc023c29a74fb5b3602410d
olragon <olragon@gmail.com>
2011-05-04 16:16:57 +0700

    1. Rename main.js -> app.js 2. Định nghĩa searchKeywords: mô tả các thông tin bài hát để tìm kiếm.

--- README.md
+++ README.md
@@ -7,5 +7,8 @@
    Homepage: http://expressjs.com
    Document: http://expressjs.com/guide.html
    Install: npm install express
-
+3. Nodemon: Monitor for any changes in your node.js application and automatically restart the server - perfect for development
+   Homepage: https://github.com/remy/nodemon
+   Document: https://github.com/remy/nodemon#readme
+   Install: sudo npm install -g nodemon
 
---
+++ backend/app.js
@@ -0,0 +1,71 @@
+var express = require('express');
+
+var app = express.createServer(
+  express.logger(),
+  express.bodyParser()
+);
+
+app.get('/', function(req, res){
+  res.send('hello world');
+});
+
+app.get('/vnmk', function(req, res){
+  res.send('Viet Nam music killer server');
+});
+
+app.get('/vnmk', function(req, res){
+  res.send('Intro');
+});
+
+app.get('/vnmk/api/?', function(req, res){
+  res.send('API document');
+});
+
+app.get('/vnmk/api/singer/:singers', function(req, res){
+  res.send('singers: ' + req.params.singers);
+});
+
+app.get('/vnmk/api/song/:songs', function(req, res){
+  res.send('songs: ' + req.params.songs);
+});
+
+app.get('/vnmk/api/writer/:writers', function(req, res){
+  res.send('writers: ' + req.params.writers);
+});
+
+app.get('/vnmk/api/album/:albums', function(req, res){
+  res.send('albums: ' + req.params.albums);
+});
+
+// app.get('/vnmk/api/search/:keywords', function(req, res){
+  // res.send('keywords: ' + req.params.keywords);
+// });
+
+app.get('/vnmk/api/search/singers=:singers?&albums=:albums', function(req, res){
+  res.send(
+    'singers: ' + req.params.singers + '\n' +
+    'albums: ' + req.params.albums + '\n'
+  );
+});
+
+/**
+ * Định nghĩa các thuộc tính để tìm kiếm bài hát.
+ *   - Chú trọng vào các thuộc tính của <b>file nhạc</b>,
+ *   các thông tin như lời bài hát,
+ *   thông tin ca sĩ, năm phát hành albums, 
+ *   ngôn ngữ ... có thể bổ sung sau.
+ */
+var searchKeywords = new Array(
+  'singers', // Tên ca sĩ. Ví dụ: Quang Dũng, Bằng Kiều
+  'albums', // Tên album. Ví dụ: The Best Of Quang Dung 1, The Best Of Quang Dung 2
+  'songs', // Tên bài hát. Ví dụ: Bâng Khuâng Chiều Nội Trú, Anh Xin Làm
+  'genes', // Thể loại nhạc. Ví dụ: Pop, Rock, ...
+  'sources', // Nguồn của file nhạc,thường là tên domain. Ví dụ: mp3.zing.vn
+  'quanlities', // Chất lượng file nhạc. Ví dụ: 32, 64, >=128 
+  'formats', // Định dạng của file nhạc. Ví dụ: mp3, wav, ogg
+  'sorts', // Tiêu chí sắp xếp. Ví dụ: 
+  'limits' // Giới hạn số lượng kết quả. Ví dụ: 1..15
+);
+
+
+app.listen(3000);
